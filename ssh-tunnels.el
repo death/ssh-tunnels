e751ad0e3ebcbfa9997c1e8f480b27fb1f787dc1
texaco <manolo.texaco@gmail.com>
2022-06-18 20:10:41 +0200

    Adds support for grouping tunnels
    Add a group name as an optional property (:group) to declared tunnels
    on `ssh-tunnels-configurations' variable.
    
    Tunnels with the same group name can be started or killed at the same
    time with `ssh-tunnels-run-group' and `ssh-tunnels-kill-group' command
    respectively.

--- ssh-tunnels.el
+++ ssh-tunnels.el
@@ -147,7 +147,11 @@ and/or `:remote-port'.
                   to the value of `:remote-socket'.
 
   :remote-socket - The tunnel's remote socket; defaults
-                   to the value of `:local-socket'."
+                   to the value of `:local-socket'.
+
+  :group - Tunnel's optional group. Set the same group to each
+           tunnel that are convenient to start together.
+"
   :type 'sexp
   :group 'ssh-tunnels)
 
@@ -455,6 +459,39 @@ or socket associated with the tunnel.")
   "Kill a running SSH tunnel."
   (interactive)
   (ssh-tunnels--kill (ssh-tunnels--read-tunnel)))
+
+(defun ssh-tunnels--read-group ()
+  (let ((candidates (seq-remove
+                      (lambda (group)
+                        (not group))    ; Removes nil value from connections without group.
+                      (seq-uniq (cl-loop
+                                 for tunnel in ssh-tunnels-configurations
+                                 collect (ssh-tunnels--property tunnel :group))))))
+    (if (not candidates)
+        (error "No tunnels with group declared on `ssh-tunnels-configurations' variable"))
+    (let ((group (completing-read "Group: " candidates nil t)))
+      (seq-filter
+     (lambda (tunnel)
+       (string= (ssh-tunnels--property tunnel :group) group))
+     ssh-tunnels-configurations))))
+
+(defun ssh-tunnels-run-group ()
+  "Start configured SSH tunnels by givin group.
+
+A group list is prompt from `ssh-tunnels-configurations' variable
+`:group' propierty given optionally to each tunnel configuration."
+  (interactive)
+  (dolist (tunnel (ssh-tunnels--read-group))
+    (ssh-tunnels--run tunnel)))
+
+(defun ssh-tunnels-kill-group ()
+  "Kill configured SSH tunnels by givin group.
+
+A group list is prompt from `ssh-tunnels-configurations' variable
+`:group' propierty given optionally to each tunnel configuration."
+  (interactive)
+  (dolist (tunnel (ssh-tunnels--read-group))
+    (ssh-tunnels--kill tunnel)))
 
 ;;; auto-ssh-tunnels mode
 
